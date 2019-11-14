5c2d19767df6620b1aee686c5e4bfdcd941d82dc
Ana Nikolova <ani.nik19@gmail.com>
2019-11-14 08:38:49 +0100

    reverse an integer

--- index.js
+++ index.js
@@ -55,3 +55,17 @@ function isPalindrome(str) {
 
   return revString === str;
 }
+
+// CHALLENGE 3: REVERSE AN INTEGER
+// Return an integer in reverse
+// ex. reverseInt(521) === 125
+
+function reverseInt(int) {
+  const revString = int
+    .toString()
+    .split("")
+    .reverse()
+    .join("");
+
+  return parseInt(revString) * Math.sign(int);
+}
