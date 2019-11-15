eb299599ac63a7d1686158edd0bd0f2c7c795a83
Ana Nikolova <ani.nik19@gmail.com>
2019-11-15 22:07:55 +0100

    capitalize letter

--- index.js
+++ index.js
@@ -34,11 +34,9 @@ function reverseString(str) {
 
   ///////////////////////
 
-  // let revString = '';
-  // str.split('').forEach(char => revString = char + revString);
-  // return revString;
-
-  //////////////////////
+  let revString = "";
+  str.split("").forEach(char => (revString = char + revString));
+  return revString;
 
   return str.split("").reduce((revString, char) => char + revString, "");
 }
@@ -68,4 +66,25 @@ function reverseInt(int) {
     .join("");
 
   return parseInt(revString) * Math.sign(int);
+}
+
+// CHALLENGE 4: CAPITALIZE LETTERS
+// Return a string with the first letter of every word capitalized
+// ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
+function capitalizeLetters(str) {
+  // const strArr = str.toLowerCase().split(' ');
+
+  // for(let i = 0; i < strArr.length; i++) {
+  //   strArr[i] = strArr[i].substring(0, 1).toUpperCase() + strArr[i].substring(1);
+  // }
+
+  // return strArr.join(' ');
+
+  /////////////////////////////
+
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map(word => word[0].toUpperCase() + word.substr(1))
+    .join(" ");
 }
