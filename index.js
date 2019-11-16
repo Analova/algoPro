9a1241353654f8d6117d40ecb2c8fa57b92b96aa
Ana Nikolova <ani.nik19@gmail.com>
2019-11-16 15:32:22 +0100

    max character

--- index.js
+++ index.js
@@ -88,3 +88,29 @@ function capitalizeLetters(str) {
     .map(word => word[0].toUpperCase() + word.substr(1))
     .join(" ");
 }
+
+// CHALLENGE 5: MAX CHARACTER
+// Return the character that is most common in a string
+// ex. maxCharacter('javascript') == 'a'
+function maxCharacter(str) {
+  const charMap = {};
+  let maxNum = 0;
+  let maxChar = '';
+
+  str.split('').forEach(function (char) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  });
+
+  for (let char in charMap) {
+    // debugger;
+    if (charMap[char] > maxNum) {
+      maxNum = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
