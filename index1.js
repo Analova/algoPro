f1ecd8d6db00bc9b898058a1eea1b5d4021a04f5
Ana Nikolova <ani.nik19@gmail.com>
2019-11-29 07:57:38 +0100

    add all numbers

--- index1.js
+++ index1.js
@@ -126,3 +126,22 @@ function letterChanges(str) {
 const output = letterChanges("Hello There");
 
 console.log(output);
+
+// CHALLENGE 1: ADD ALL NUMBERS
+// Return a sum of all parameters entered regardless of the amount of numbers - NO ARRAYS
+// ex. addAll(2,5,6,7) === 20
+
+// Solution 1: ES5 arguments & for loop
+function addAll() {
+  var args = Array.prototype.slice.call(arguments);
+  var total = 0;
+  for (let i = 0; i < args.length; i++) {
+    total += args[i];
+  }
+  return total;
+}
+
+// Solution 2: ...rest & reduce/forEach
+function addAll(...numbers) {
+  return numbers.reduce((acc, cur) => acc + cur);
+}
