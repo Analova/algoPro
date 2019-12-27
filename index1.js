d82b92dbc725f84fa646b8d2c477bfe3ff7642b3
Ana Nikolova <ani.nik19@gmail.com>
2019-12-27 20:27:27 +0200

    seek and destroy

--- index1.js
+++ index1.js
@@ -190,3 +190,24 @@ function isValidPassword(password, username) {
   const tooSimilar = password.indexOf(username) !== -1;
   return !tooShort && !hasSpace && !tooSimilar;
 }
+
+// CHALLENGE 3: SEEK & DESTROY
+// Remove from the array whatever is in the following arguments. Return the leftover values in an array
+// ex. seekAndDestroy([2, 3, 4, 6, 6, 'hello'], 2, 6) == [3, 4, 'hello']
+
+// Solution 1: arguments, indexOf, filter
+function seekAndDestroy(arr) {
+  const args = Array.from(arguments);
+
+  function filterArr(arr) {
+    // Return true if NOT in array
+    return args.indexOf(arr) === -1;
+  }
+
+  return arr.filter(filterArr);
+}
+
+// Solution 2: ...rest, filter & includes
+function seekAndDestroy(arr, ...rest) {
+  return arr.filter(val => !rest.includes(val));
+}
