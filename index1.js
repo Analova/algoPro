640502e35e5c936f14170f6e9a5b30a07715c9db
Ana Nikolova <ani.nik19@gmail.com>
2020-01-01 18:28:58 +0100

    sort by height

--- index1.js
+++ index1.js
@@ -191,7 +191,7 @@ function isValidPassword(password, username) {
   return !tooShort && !hasSpace && !tooSimilar;
 }
 
-// CHALLENGE 3: SEEK & DESTROY
+//  SEEK & DESTROY
 // Remove from the array whatever is in the following arguments. Return the leftover values in an array
 // ex. seekAndDestroy([2, 3, 4, 6, 6, 'hello'], 2, 6) == [3, 4, 'hello']
 
@@ -210,4 +210,23 @@ function seekAndDestroy(arr) {
 // Solution 2: ...rest, filter & includes
 function seekAndDestroy(arr, ...rest) {
   return arr.filter(val => !rest.includes(val));
+}
+
+//  SORT BY HEIGHT
+// Some people are standing in a row in a park. There are trees between them which cannot be moved. Your task is to rearrange the people by their heights in a non-descending order without moving the trees.
+// ex.
+// a = [-1, 150, 190, 170, -1, -1, 160, 180]
+// sortByHeight(a) == [-1, 150, 160, 170, -1, -1, 180, 190]
+
+function sortByHeight(a) {
+  const arr1 = [];
+  const arr2 = [];
+
+  a.forEach((val, i) => (val === -1 ? arr1.push(i) : arr2.push(val)));
+
+  const sortArr = arr2.sort((a, b) => a - b);
+
+  arr1.forEach((val, i) => sortArr.splice(arr1[i], 0, -1));
+
+  return sortArr;
 }
